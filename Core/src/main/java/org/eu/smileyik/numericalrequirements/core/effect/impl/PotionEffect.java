ed8564e027535687a7d96987e340af658547f06a
Smile Yik <miskyle@outlook.com>
2024-07-10 23:25:33 +0800

    PotionEffect: 改变Data类为内部类，修改更新频率为980毫秒一次

--- Core/src/main/java/org/eu/smileyik/numericalrequirements/core/effect/impl/PotionEffect.java
+++ Core/src/main/java/org/eu/smileyik/numericalrequirements/core/effect/impl/PotionEffect.java
@@ -5,10 +5,13 @@ import org.bukkit.potion.PotionEffectType;
 import org.eu.smileyik.numericalrequirements.core.I18N;
 import org.eu.smileyik.numericalrequirements.core.api.AbstractRegisterInfo;
 import org.eu.smileyik.numericalrequirements.core.api.NumericalRequirements;
+import org.eu.smileyik.numericalrequirements.core.api.effect.AbstractEffectData;
 import org.eu.smileyik.numericalrequirements.core.api.effect.Effect;
 import org.eu.smileyik.numericalrequirements.core.api.effect.EffectData;
 import org.eu.smileyik.numericalrequirements.core.api.player.NumericalPlayer;
 import org.eu.smileyik.numericalrequirements.core.api.player.PlayerDataValue;
+
+import java.util.Objects;
 
 public class PotionEffect extends AbstractRegisterInfo implements Effect {
 
@@ -74,6 +77,65 @@ public class PotionEffect extends AbstractRegisterInfo implements Effect {
                     player.getPlayer().addPotionEffect(potionEffect);
                 });
             //}
+        }
+    }
+
+    public static class PotionEffectData extends AbstractEffectData {
+        private String potionType;
+        private int amplifier;
+        private org.bukkit.potion.PotionEffect potionEffect;
+
+        public int getAmplifier() {
+            return amplifier;
+        }
+
+        public void setAmplifier(int amplifier) {
+            this.amplifier = amplifier;
+        }
+
+        public String getPotionType() {
+            return potionType;
+        }
+
+        public void setPotionType(String potionType) {
+            this.potionType = potionType;
+        }
+
+        @Override
+        public void store(ConfigurationSection section) {
+            super.store(section);
+            section.set("potion-type", potionType);
+            section.set("amplifier", amplifier);
+        }
+
+        @Override
+        public void load(ConfigurationSection section) {
+            super.load(section);
+            potionType = section.getString("potion-type");
+            amplifier = section.getInt("amplifier");
+        }
+
+        public org.bukkit.potion.PotionEffect getPotionEffect() {
+            if (potionEffect == null) {
+                if (potionType != null) {
+                    PotionEffectType type = PotionEffectType.getByName(potionType);
+                    if (type != null) {
+                        potionEffect = type.createEffect(100, amplifier);
+                    }
+                }
+            }
+            return potionEffect;
+        }
+
+        @Override
+        public long period() {
+            return 980;
+        }
+
+        @Override
+        public boolean isSimilar(EffectData other) {
+            return (other instanceof PotionEffectData) &&
+                    Objects.equals(((PotionEffectData) other).potionType, this.potionType);
         }
     }
 }
