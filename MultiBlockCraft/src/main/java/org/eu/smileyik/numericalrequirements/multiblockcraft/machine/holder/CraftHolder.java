9f5f3e885962e8190b3e4950725443245daee30a
Smile Yik <miskyle@outlook.com>
2024-07-15 08:18:33 +0800

    SimpleCraftHolder: 更新接口使得能获取 MachineData

--- MultiBlockCraft/src/main/java/org/eu/smileyik/numericalrequirements/multiblockcraft/machine/holder/CraftHolder.java
+++ MultiBlockCraft/src/main/java/org/eu/smileyik/numericalrequirements/multiblockcraft/machine/holder/CraftHolder.java
@@ -1,9 +1,13 @@
 package org.eu.smileyik.numericalrequirements.multiblockcraft.machine.holder;
 
 import org.bukkit.inventory.InventoryHolder;
+import org.eu.smileyik.numericalrequirements.multiblockcraft.data.MachineData;
 import org.eu.smileyik.numericalrequirements.multiblockcraft.machine.Machine;
 
 public interface CraftHolder extends InventoryHolder {
     Machine getMachine();
     String getIdentifier();
+    default MachineData getMachineData() {
+        return null;
+    }
 }
--- MultiBlockCraft/src/main/java/org/eu/smileyik/numericalrequirements/multiblockcraft/machine/holder/SimpleCraftHolder.java
+++ MultiBlockCraft/src/main/java/org/eu/smileyik/numericalrequirements/multiblockcraft/machine/holder/SimpleCraftHolder.java
@@ -1,12 +1,14 @@
 package org.eu.smileyik.numericalrequirements.multiblockcraft.machine.holder;
 
 import org.bukkit.inventory.Inventory;
+import org.eu.smileyik.numericalrequirements.multiblockcraft.data.MachineData;
 import org.eu.smileyik.numericalrequirements.multiblockcraft.machine.Machine;
 
 public class SimpleCraftHolder implements CraftHolder {
     private Machine machine;
     private String identifier;
     private Inventory inventory;
+    private MachineData machineData;
 
     public SimpleCraftHolder() {
 
@@ -22,6 +24,15 @@ public class SimpleCraftHolder implements CraftHolder {
 
     public void setMachine(Machine machine) {
         this.machine = machine;
+    }
+
+    public void setMachineData(MachineData machineData) {
+        this.machineData = machineData;
+    }
+
+    @Override
+    public MachineData getMachineData() {
+        return machineData;
     }
 
     @Override
