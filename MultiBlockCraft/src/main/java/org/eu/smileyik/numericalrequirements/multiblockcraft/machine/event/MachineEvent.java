0c5d9bffbdeef047237b735ad2aa8cf6a1aef678
Smile Yik <miskyle@outlook.com>
2024-07-24 22:33:32 +0800

    MultiBlockCraft: 修复异步触发事件问题

--- MultiBlockCraft/src/main/java/org/eu/smileyik/numericalrequirements/multiblockcraft/machine/event/MachineEvent.java
+++ MultiBlockCraft/src/main/java/org/eu/smileyik/numericalrequirements/multiblockcraft/machine/event/MachineEvent.java
@@ -1,12 +1,14 @@
 package org.eu.smileyik.numericalrequirements.multiblockcraft.machine.event;
 
 import org.eu.smileyik.numericalrequirements.core.event.Event;
+import org.eu.smileyik.numericalrequirements.multiblockcraft.MultiBlockCraftExtension;
 import org.eu.smileyik.numericalrequirements.multiblockcraft.machine.Machine;
 
 public class MachineEvent extends Event {
     private final Machine machine;
 
     public MachineEvent(Machine machine) {
+        super(!MultiBlockCraftExtension.getInstance().getPlugin().getServer().isPrimaryThread());
         this.machine = machine;
     }
 
