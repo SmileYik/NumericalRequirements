ceb7544a5ecd6b497069a8b052b9c30880ea2b64
Smile Yik <miskyle@outlook.com>
2024-07-25 22:22:57 +0800

    NBTTag: 添加新方法以方便获取物品中的值

--- Core/src/main/java/org/eu/smileyik/numericalrequirements/core/api/item/tag/nbt/NBTTag.java
+++ Core/src/main/java/org/eu/smileyik/numericalrequirements/core/api/item/tag/nbt/NBTTag.java
@@ -3,6 +3,8 @@ package org.eu.smileyik.numericalrequirements.core.api.item.tag.nbt;
 import org.bukkit.inventory.ItemStack;
 import org.eu.smileyik.numericalrequirements.core.api.item.tag.ItemTag;
 import org.eu.smileyik.numericalrequirements.nms.nbt.NBTTagCompound;
+import org.eu.smileyik.numericalrequirements.nms.nbtitem.NBTItem;
+import org.eu.smileyik.numericalrequirements.nms.nbtitem.NBTItemHelper;
 
 import java.util.List;
 
@@ -25,4 +27,16 @@ public abstract class NBTTag <V> implements ItemTag<V> {
     public abstract boolean isValidValue(List<String> value);
 
     public abstract void setValue(ItemStack itemStack, List<String> value);
+
+    /**
+     * 从物品中获取值。
+     * @param itemStack
+     * @return
+     */
+    public V getValue(ItemStack itemStack) {
+        NBTItem cast = NBTItemHelper.cast(itemStack);
+        if (cast == null) return null;
+        NBTTagCompound tag = cast.getTag();
+        return tag == null ? null : getValue(tag);
+    }
 }
