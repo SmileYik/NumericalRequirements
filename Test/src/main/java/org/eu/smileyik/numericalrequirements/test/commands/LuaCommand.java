fb05a80f7a34a9dfe4c8767aaa905d1d6cd2d979
Smile Yik <miskyle@outlook.com>
2024-08-01 16:36:21 +0800

    LuaCommand: 初始化Lua状态时加入task全局变量以运行同步线程

--- Test/src/main/java/org/eu/smileyik/numericalrequirements/test/commands/LuaCommand.java
+++ Test/src/main/java/org/eu/smileyik/numericalrequirements/test/commands/LuaCommand.java
@@ -1,8 +1,11 @@
 package org.eu.smileyik.numericalrequirements.test.commands;
 
+import org.bukkit.scheduler.BukkitTask;
 import org.eu.smileyik.numericalrequirements.core.api.NumericalRequirements;
 import org.eu.smileyik.numericalrequirements.debug.DebugLogger;
 import org.eu.smileyik.numericalrequirements.test.TestCommand;
+import org.keplerproject.luajava.LuaException;
+import org.keplerproject.luajava.LuaObject;
 import org.keplerproject.luajava.LuaState;
 import org.keplerproject.luajava.LuaStateFactory;
 import tk.smileyik.luainminecraftbukkit.util.luahelper.LuaHelper;
@@ -24,6 +27,8 @@ public class LuaCommand {
             luaState.setGlobal("helper");
             luaState.pushJavaObject(NumericalRequirements.getInstance());
             luaState.setGlobal("nr");
+            luaState.pushJavaObject(new Task(luaState));
+            luaState.setGlobal("task");
             System.out.println("Lua Console Started");
             return;
         } else if (str.equals("close console")) {
@@ -38,6 +43,26 @@ public class LuaCommand {
         }
         if (ret != 0) {
             System.out.println("Run " + str + " failed: " + luaState.toString(-1));
+        }
+    }
+
+    public static class Task {
+        private final LuaState luaState;
+
+        public Task(LuaState luaState) {
+            this.luaState = luaState;
+        }
+
+        public BukkitTask runTask(Object object) {
+            return NumericalRequirements.getPlugin().getServer().getScheduler().runTask(NumericalRequirements.getPlugin(), () -> {
+                if (object instanceof LuaObject) {
+                    try {
+                        ((LuaObject) object).call(new Object[] {});
+                    } catch (LuaException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
         }
     }
 }
