3a462e7baff5a2db6a3a64c4ad2acb43aff2df8d
Smile Yik <miskyle@outlook.com>
2024-08-03 12:53:28 +0800

    NetworkService: 增加发布数据包与广播数据包接口 & NetworkServiceImpl: 修复玩家退出后再进服务器时Handler无效问题

--- Core/src/main/java/org/eu/smileyik/numericalrequirements/core/network/NetworkService.java
+++ Core/src/main/java/org/eu/smileyik/numericalrequirements/core/network/NetworkService.java
@@ -1,8 +1,51 @@
 package org.eu.smileyik.numericalrequirements.core.network;
 
+import org.bukkit.entity.Player;
+import org.eu.smileyik.numericalrequirements.nms.network.packet.Packet;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+
 public interface NetworkService {
+    /**
+     * 给指定玩家发送数据包.
+     * @param player
+     * @param packet
+     */
+    void sendPacket(Player player, Packet packet);
+
+    /**
+     * 给指定玩家发送数据包.
+     * @param player
+     * @param packet
+     */
+    void sendPacket(Player player, Object packet);
+
+    /**
+     * 广播数据包.
+     * @param packet
+     */
+    void broadcastPacket(Packet packet);
+
+    /**
+     * 广播数据包.
+     * @param packet
+     */
+    void broadcastPacket(Object packet);
+
+    /**
+     * 广播数据包给指定玩家.
+     * @param playerList
+     * @param packet
+     */
+    void broadcastPacket(Collection<Player> playerList, Packet packet);
+
+    /**
+     * 广播数据包给指定玩家.
+     * @param playerList
+     * @param packet
+     */
+    void broadcastPacket(Collection<Player> playerList, Object packet);
 
     /**
      * 添加包监听器
--- Core/src/main/java/org/eu/smileyik/numericalrequirements/core/network/NetworkServiceImpl.java
+++ Core/src/main/java/org/eu/smileyik/numericalrequirements/core/network/NetworkServiceImpl.java
@@ -1,5 +1,6 @@
 package org.eu.smileyik.numericalrequirements.core.network;
 
+import io.netty.channel.Channel;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,8 +8,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.eu.smileyik.numericalrequirements.core.NumericalRequirements;
 import org.eu.smileyik.numericalrequirements.debug.DebugLogger;
 import org.eu.smileyik.numericalrequirements.nms.NMSNetwork;
+import org.eu.smileyik.numericalrequirements.nms.network.packet.Packet;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -29,11 +32,53 @@ public class NetworkServiceImpl implements Listener, NetworkService {
     }
 
     private synchronized void listen(Player player) {
-        if (playerChannelHandlers.containsKey(player.getName())) return;
+        if (playerChannelHandlers.containsKey(player.getName())) {
+            Channel channel = playerChannelHandlers.get(player.getName()).getChannel();
+            if (channel.isOpen()) return;
+            playerChannelHandlers.remove(player.getName());
+        }
         PlayerChannelHandler playerChannelHandler = new PlayerChannelHandler();
         if (NMSNetwork.registerChannelListener(player, "nreq-core-listener", playerChannelHandler)) {
             DebugLogger.debug("Registered channel listener for player: %s", player.getName());
             playerChannelHandlers.putIfAbsent(player.getName(), playerChannelHandler);
+        }
+    }
+
+    private synchronized void unlisten(Player player) {
+        playerChannelHandlers.remove(player.getName());
+    }
+
+    @Override
+    public synchronized void sendPacket(Player player, Packet packet) {
+        this.sendPacket(player, packet.getInstance());
+    }
+
+    @Override
+    public synchronized void sendPacket(Player player, Object packet) {
+        if (playerChannelHandlers.containsKey(player.getName())) {
+            playerChannelHandlers.get(player.getName()).sendPacket(packet);
+        }
+    }
+
+    @Override
+    public synchronized void broadcastPacket(Packet packet) {
+        this.broadcastPacket(packet.getInstance());
+    }
+
+    @Override
+    public synchronized void broadcastPacket(Object packet) {
+        playerChannelHandlers.forEach((name, channelHandler) -> channelHandler.sendPacket(packet));
+    }
+
+    @Override
+    public synchronized void broadcastPacket(Collection<Player> playerList, Packet packet) {
+        this.broadcastPacket(playerList, packet.getInstance());
+    }
+
+    @Override
+    public synchronized void broadcastPacket(Collection<Player> playerList, Object packet) {
+        for (Player player : playerList) {
+            this.sendPacket(player, packet);
         }
     }
 
