eba7cc4701f4166e9656a89a4cf98946e3c32593
Smile Yik <miskyle@outlook.com>
2024-08-03 12:54:42 +0800

    NMS: 更新包装基类注释文档

--- NMS/src/main/java/org/eu/smileyik/numericalrequirements/nms/ReflectClassBase.java
+++ NMS/src/main/java/org/eu/smileyik/numericalrequirements/nms/ReflectClassBase.java
@@ -1,5 +1,13 @@
 package org.eu.smileyik.numericalrequirements.nms;
 
+/**
+ * 反射包装类.
+ */
 public interface ReflectClassBase {
+
+    /**
+     * 获取原类型实例.
+     * @return
+     */
     Object getInstance();
 }
